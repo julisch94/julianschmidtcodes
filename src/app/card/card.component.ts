f7ab5b3d5deefc1470c5ce77a44e3b2442883e14
Julian Schmidt <julian.schmidt@nexusgroup.com>
2020-11-26 22:37:35 +0100

    create copy icon; copy mail to clipboard on click

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  mail = 'contact@julianschmidt.codes';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  copyMailToClipboard(): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.mail;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
